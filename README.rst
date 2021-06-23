1e301716e0e39c2845e2d310256fddac27a1b0e9
Kevin Wierman <kwierman@gmail.com>
2021-06-23 16:04:18 -0700

    first commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+===============================
+manybody
+===============================
+
+
+.. image:: https://img.shields.io/travis/kwierman/manybody.svg
+        :target: https://travis-ci.org/kwierman/manybody
+.. image:: https://circleci.com/gh/kwierman/manybody.svg?style=svg
+    :target: https://circleci.com/gh/kwierman/manybody
+.. image:: https://codecov.io/gh/kwierman/manybody/branch/master/graph/badge.svg
+   :target: https://codecov.io/gh/kwierman/manybody
+
+
+MLOps Design Patterns for Many Bodies
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    # package requirements go here
+]
+
+setup(
+    name='manybody',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="MLOps Design Patterns for Many Bodies",
+    license="MIT",
+    author="Kevin Wierman",
+    author_email='kwierman@gmail.com',
+    url='https://github.com/kwierman/manybody',
+    packages=['manybody'],
+    
+    install_requires=requirements,
+    keywords='manybody',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
